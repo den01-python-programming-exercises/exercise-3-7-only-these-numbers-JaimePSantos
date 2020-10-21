180092305936a6f7c58e7c0baa1c2d2e95b6e6a9
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 20:38:01 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    intList = []
+    while(True):
+      number = int(input())
+      if(number==-1):
+        break
+      intList.append(number)
+    from1 = int(input("From where?"))
+    to1 = int(input("To where?"))
+    for i in range(from1,to1+1):
+      print(intList[i])
+    
+
+
 
 if __name__ == '__main__':
     main()
